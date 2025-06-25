7abe60672ff2b657ded443148e390fe28773bca3
dbmurray <darragh.murray@gmail.com>
2025-06-25 23:29:59 +1000

    get rid of this crap

--- install-quarto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Download and install Quarto CLI
-wget https://quarto.org/download/latest/quarto-linux-amd64.deb
-sudo dpkg -i quarto-linux-amd64.deb
-
-# Clean up
-rm quarto-linux-amd64.deb
